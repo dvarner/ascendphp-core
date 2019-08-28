d842405e6785afd72785e33ac44a06d10d311f9b
David Varner <dvarner@ascendphp.com>
2019-08-28 01:15:16 -0500

    Removing no longer needed route functionality

--- src/Ascend/Core/Route.php
+++ src/Ascend/Core/Route.php
@@ -2,40 +2,6 @@
 
 class Route
 {
-    public static function sendWhereFrameworkBeingUsed()
-    {
-        $e = explode('/api', self::getURI());
-        if (!isset($e[1])) {
-            $url = 'https://framework.ldev.work/api/domain/' . DOMAIN;
-
-            $options = array(
-                CURLOPT_RETURNTRANSFER => true,     // return web page
-                CURLOPT_HEADER => false,    // don't return headers
-                CURLOPT_FOLLOWLOCATION => true,     // follow redirects
-                CURLOPT_ENCODING => "",       // handle all encodings
-                CURLOPT_USERAGENT => "framework", // who am i
-                CURLOPT_AUTOREFERER => true,     // set referer on redirect
-                CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
-                CURLOPT_TIMEOUT => 120,      // timeout on response
-                CURLOPT_MAXREDIRS => 10,       // stop after 10 redirects
-                CURLOPT_SSL_VERIFYPEER => false,     // Disabled SSL Cert checks
-            );
-
-            $ch = curl_init($url);
-            curl_setopt_array($ch, $options);
-            $content = curl_exec($ch);
-            $err = curl_errno($ch);
-            $errmsg = curl_error($ch);
-            $header = curl_getinfo($ch);
-            curl_close($ch);
-
-            $header['errno'] = $err;
-            $header['errmsg'] = $errmsg;
-            $header['content'] = $content;
-            return $header;
-        }
-    }
-
     public static function getURI()
     {
         $uri = $_SERVER['REQUEST_URI'];
